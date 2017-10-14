76e846b5c8ac7589c22831ac0e261deb459673ba
Shadid Haque <shaque@intouchinsight.com>
2017-10-14 17:26:19 -0400

    functionality working

---
+++ App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import { BarCodeScanner, Permissions } from 'expo';
+import axios from 'axios';
+
+export default class BarcodeScannerExample extends React.Component {
+  state = {
+    hasCameraPermission: null,
+  }
+
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({hasCameraPermission: status === 'granted'});
+    }
+
+  render() {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <BarCodeScanner
+            onBarCodeRead={this._handleBarCodeRead}
+            style={StyleSheet.absoluteFill}
+          />
+          <Text>Some Text</Text>
+        </View>
+      );
+    }
+  }
+
+  _cancel() {
+    console.log("Pressed Cancel");
+  }
+
+  _sendData(data) {
+        axios.post('https://shadid12.herokuapp.com/item', {
+          geocode: data
+        })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+  }
+
+  _handleBarCodeRead = ({ type, data }) => {
+    Alert.alert(
+      'Barcode Scanned',
+      `Bar code with type ${type} and data ${data} has been scanned!`,
+      [
+        {text: 'Cancel', onPress: () => this._cancel()},
+        {text: 'Send', onPress: () => this._sendData(data)},
+      ],
+      { cancelable: false }
+    )
+  }
+}
---
+++ components/ShowSend.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class ShowSend extends Component {
+	constructor(props){
+		super(props);
+	}
+
+	render() {
+		return(
+			<Text>Some other Stuff</Text>
+		)
+	}
+}
+
+export default ShowSend;
